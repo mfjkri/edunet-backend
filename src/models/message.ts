14fcfde123cc5cc030bb674d134842ae7e9336f8
mfjkri <muhammadfikri6844@gmail.com>
2023-09-25 19:10:35 +0800

    Disable messages

--- src/models/message.ts
+++ src/models/message.ts
@@ -1,95 +1,95 @@
-import {
-  Association,
-  CreationOptional,
-  DataTypes,
-  InferAttributes,
-  InferCreationAttributes,
-  Model,
-  Sequelize,
-} from "sequelize";
+// import {
+//   Association,
+//   CreationOptional,
+//   DataTypes,
+//   InferAttributes,
+//   InferCreationAttributes,
+//   Model,
+//   Sequelize,
+// } from "sequelize";
 
-import getDB from "../database/database";
-import Centre from "./centre";
-import User from "./user";
+// import getDB from "../database/database";
+// import Centre from "./centre";
+// import User from "./user";
 
-export default class Note extends Model<
-  InferAttributes<Note>,
-  InferCreationAttributes<Note>
-> {
-  declare id: CreationOptional<number>;
+// export default class Note extends Model<
+//   InferAttributes<Note>,
+//   InferCreationAttributes<Note>
+// > {
+//   declare id: CreationOptional<number>;
 
-  declare centreId: number;
-  declare senderId: number;
-  declare receiverId: number;
+//   declare centreId: number;
+//   declare senderId: number;
+//   declare receiverId: number;
 
-  declare title: string;
-  declare content: string;
+//   declare title: string;
+//   declare content: string;
 
-  declare createdAt: CreationOptional<Date>;
-  declare updatedAt: CreationOptional<Date>;
+//   declare createdAt: CreationOptional<Date>;
+//   declare updatedAt: CreationOptional<Date>;
 
-  declare readonly centre?: Centre;
-  declare readonly user?: User;
+//   declare readonly centre?: Centre;
+//   declare readonly user?: User;
 
-  declare static associations: {
-    centre: Association<Note, Centre>;
-    user: Association<Note, User>;
-  };
-}
+//   declare static associations: {
+//     centre: Association<Note, Centre>;
+//     user: Association<Note, User>;
+//   };
+// }
 
-export function init(db?: Sequelize) {
-  Note.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
+// export function init(db?: Sequelize) {
+//   Note.init(
+//     {
+//       id: {
+//         type: DataTypes.INTEGER,
+//         primaryKey: true,
+//         autoIncrement: true,
+//       },
 
-      centreId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
-      userId: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-      },
+//       centreId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//       },
+//       userId: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//       },
 
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+//       title: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
+//       content: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//       },
 
-      createdAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-      updatedAt: {
-        type: DataTypes.DATE,
-        allowNull: false,
-        defaultValue: DataTypes.NOW,
-      },
-    },
-    { sequelize: db || getDB() }
-  );
+//       createdAt: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//         defaultValue: DataTypes.NOW,
+//       },
+//       updatedAt: {
+//         type: DataTypes.DATE,
+//         allowNull: false,
+//         defaultValue: DataTypes.NOW,
+//       },
+//     },
+//     { sequelize: db || getDB() }
+//   );
 
-  Note.belongsTo(Centre, {
-    foreignKey: "centreId",
-    onDelete: "CASCADE",
-  });
+//   Note.belongsTo(Centre, {
+//     foreignKey: "centreId",
+//     onDelete: "CASCADE",
+//   });
 
-  Note.belongsTo(User, {
-    foreignKey: "userId",
-    onDelete: "CASCADE",
-  });
+//   Note.belongsTo(User, {
+//     foreignKey: "userId",
+//     onDelete: "CASCADE",
+//   });
 
-  User.hasMany(Note, {
-    foreignKey: "userId",
-    as: "notes",
-  });
-}
+//   User.hasMany(Note, {
+//     foreignKey: "userId",
+//     as: "notes",
+//   });
+// }
