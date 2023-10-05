ea3e18448829557c46e0ea4b5863f77069994c19
mfjkri <muhammadfikri6844@gmail.com>
2023-10-06 01:07:21 +0800

    Fix max stack size error

--- src/routes/public/public.ts
+++ src/routes/public/public.ts
@@ -7,19 +7,18 @@ const router: Router = Router();
 
 router.use("/public", AuthRouter);
 
-router.use(
-  router.get("/ping", async (req, res) => {
-    return res.status(200).json({ data: "pong" });
-  }),
-  router.post("/ingredients", async (req, res) => {
-    try {
-      const rest = await getLabels(req.body.base64_string);
-      return res.status(200).json({ data: rest });
-    } catch (error: any) {
-      console.log("Ingredients Error: ", error);
-      return res.status(500).json({ error: "Internal Server Error" });
-    }
-  })
-);
+router.get("/ping", async (req, res) => {
+  return res.status(200).json({ data: "pong" });
+});
+
+router.post("/ingredients", async (req, res) => {
+  try {
+    const rest = await getLabels(req.body.base64_string);
+    return res.status(200).json({ data: rest });
+  } catch (error: any) {
+    console.log("Ingredients Error: ", error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 export default router;
